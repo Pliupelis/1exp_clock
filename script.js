98e46becc70a8f2fe87052bec5cc8fefe9fd217e
pliupelis <pliupelis@inbox.lt>
2021-09-26 00:38:14 +0300

    fix'ed time update

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 
 document.addEventListener("DOMContentLoaded", function(event){
 
+
+
+
+setInterval( ()=>{
 const d = new Date();
 const time = d.toLocaleTimeString('en-IT', { hour12: false });
-
 document.getElementById('time').innerHTML = time;
-
+},1000);
 
 });
