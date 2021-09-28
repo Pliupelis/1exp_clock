2a9b6b9e35b97472e48474fdaf3cfafb3084e916
pliupelis <pliupelis@inbox.lt>
2021-09-28 19:27:50 +0300

    working on buttons//start

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
 
 document.addEventListener("DOMContentLoaded", function(event){
-
+let zeroTime = 0;
 setInterval( ()=>{
 const d = new Date();
+const display = document.getElementById('time');
+const container = document.getElementById('box');
 const time = d.toLocaleTimeString('en-IT', { hour12: false });
-document.getElementById('time').innerHTML = time;
+display.innerHTML = time;
+
+
 },1000);
 
+
 });
+
+/*
+updateTime = () =>{
+  const isButton = event.target.nodeName === 'BUTTON';
+if (!isButton) {
+  return;
+}
+zeroTime ++;
+display.innerHTML = zeroTime;
+};
+
+container.addEventListener('click', ()=>{
+setInterval(updateTime(),1000);
+console.log("asa");
+}); */
